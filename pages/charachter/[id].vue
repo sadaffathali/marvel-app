1232eee3983cf44482f2faa02e019269fa17e7a1
sadaf <fathali.sadaf@gmail.com>
2023-05-13 20:20:48 +0330

    add charachter path and validate it

---
+++ pages/charachter/[id].vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script setup>
+definePageMeta({
+    path: "/charachter/:id",
+    validate: (route) => /^\d+$/.test(route.params.id),
+});
+
+</script>
